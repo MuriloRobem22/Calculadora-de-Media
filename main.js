b93bbba75ab578cfbfcd9e775e52d6f38fb40bf7
murilo <murimagal@outlook.com>
2026-01-20 22:58:03 -0300

    Adicionando arquivos do projeto

---
+++ main.js
@@ -0,0 +1,48 @@
+//var Media regular //
+
+let notaProva = document.getElementById("notaProva");
+let notaAtividades = document.getElementById("notaAtividades");
+let resultadoRegular = document.getElementById("resultadoRegular");
+let mostrarResultado = document.getElementById("mostrarResultado");
+
+
+// Media Regular Função//
+function calcularMediaRegular() {
+    let prova = parseFloat(notaProva.value);
+    let Atividades = parseFloat(notaAtividades.value);
+    let mediaRegular = (prova * 0.6 + Atividades * 0.4);
+
+    if (mediaRegular >= 5.0) {
+        mostrarResultado.innerHTML = "<p> Parabéns! Você foi aprovado com média: " + mediaRegular.toFixed(1) + "</p>";
+    } else {
+        mostrarResultado.innerHTML = "<p class='mediaBaixa' id='resultadoIntervalo'> Infelizmente você foi reprovado com média: " + mediaRegular.toFixed(1) + "</p>";
+    }
+
+    
+}
+
+    resultadoRegular.addEventListener("click", calcularMediaRegular);
+    notaAtividades.addEventListener("keydown", function(event) 
+        {   
+            if (event.key === "Enter") 
+                {
+                    if (notaProva.value !== "" && notaAtividades.value !== ""){
+                calcularMediaRegular();}
+                else {
+                alert("Por favor, preencha ambos os campos de nota antes de pressionar Enter.");
+            }
+                }
+            
+        });
+    notaProva.addEventListener("keydown", function(event) 
+                {   
+            if (event.key === "Enter") 
+                {
+                    if (notaProva.value !== "" && notaAtividades.value !== ""){
+                calcularMediaRegular();}
+                else {
+                alert("Por favor, preencha ambos os campos de nota antes de pressionar Enter.");
+            }
+                }
+            
+        });
---
+++ mediaExame.js
@@ -0,0 +1,48 @@
+//var Media Exame //
+let resultadoExame = document.getElementById("resultadoExame");
+let notaExame = document.getElementById("notaExame");
+let mediaFinal = document.getElementById("mediaFinal");
+let mostrarResultado = document.getElementById("mostrarResultadoExame");
+
+// Media Exame Função//
+
+
+function calcularMediaExame() {
+
+    let mFinal = parseFloat(mediaFinal.value);
+    let Exame = parseFloat(notaExame.value);
+    let mediaExame = (mFinal + Exame) / 2;
+
+    if (mediaExame >= 5.0) {
+        mostrarResultado.innerHTML = "<p> Parabéns! Você foi aprovado com média: " + mediaExame.toFixed(1) + "</p>";
+    } else {
+        mostrarResultado.innerHTML = "<p class='mediaBaixa'> Infelizmente você foi reprovado com média: " + mediaExame.toFixed(1) + "</p>";
+    }
+
+}
+
+resultadoExame.addEventListener("click", calcularMediaExame);
+
+mediaFinal.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        if (mediaFinal.value !== "" && notaExame.value !== "") {
+            calcularMediaExame();
+        }
+        else {
+            alert("Por favor, preencha ambos os campos de nota antes de pressionar Enter.");
+        }
+    }
+
+});
+
+notaExame.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        if (mediaFinal.value !== "" && notaExame.value !== "") {
+            calcularMediaExame();
+        }
+        else {
+            alert("Por favor, preencha ambos os campos de nota antes de pressionar Enter.");
+        }
+    }
+
+});
