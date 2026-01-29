d5eeeb28cd3eb54433b47415150cf13063fa823a
murilo <murimagal@outlook.com>
2026-01-29 02:40:58 -0300

    Projetando armazenamento de hitorico de caldulos de medias com localStorage. parte do JS pronta e já é possivel ver o historico pelo console do navegador, pendente apenas implementação no corpo da pagina

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let notaProva = document.getElementById("notaProva");
 let notaAtividades = document.getElementById("notaAtividades");
 let resultadoRegular = document.getElementById("resultadoRegular");
 let mostrarResultado = document.getElementById("mostrarResultado");
+let historico = JSON.parse(localStorage.getItem('mediaRegularStorage')) || [];
+let puxarHistorico = JSON.parse(localStorage.getItem('mediaRegularStorage'))
 
 
 // Media Regular Função//
@@ -11,6 +13,17 @@ function calcularMediaRegular() {
     let prova = parseFloat(notaProva.value);
     let Atividades = parseFloat(notaAtividades.value);
     let mediaRegular = (prova * 0.6 + Atividades * 0.4);
+    let registro = {
+        media: mediaRegular,
+        data: new Date().toLocaleDateString(),
+        hora: new Date().toLocaleTimeString(),
+    };
+
+    //adicionando o valor do imput na lista
+    historico.push(registro);
+    //mandando o valor para o localstorage
+    localStorage.setItem('mediaRegularStorage', JSON.stringify(historico))
+
 
     if (mediaRegular >= 5.0) {
         mostrarResultado.innerHTML = "<p> Parabéns! Você foi aprovado com média: " + mediaRegular.toFixed(1) + "</p>";
@@ -23,6 +36,16 @@ function calcularMediaRegular() {
         mostrarResultado.innerHTML = "<p class='mediaBaixa' id='resultadoIntervalo'> Infelizmente você foi reprovado com média: " + mediaRegular.toFixed(1) + "</p>";
     }
 
+
+    historico.forEach((registro, index) => { 
+    let resultado = registro.media >= 5.0 ? "Foi aprovado" : "Foi reprovado"
+    console.log(`Cálculo ${index + 1}: 
+        Média = ${registro.media}
+        Data = ${registro.data}
+        Hora = ${registro.hora}
+        resultado = ${resultado}`
+    ); 
+});
 
 }
 
--- mediaExame.js
+++ mediaExame.js
@@ -3,8 +3,11 @@ let resultadoExame = document.getElementById("resultadoExame");
 let notaExame = document.getElementById("notaExame");
 let mediaFinal = document.getElementById("mediaFinal");
 let mostrarResultado = document.getElementById("mostrarResultadoExame");
+let historico = JSON.parse(localStorage.getItem('mediaExameStorage')) || [];
+let puxarHistorico = JSON.parse(localStorage.getItem('mediaExameStorage'))
 
 // Media Exame Função//
+
 
 
 function calcularMediaExame() {
@@ -14,8 +17,20 @@ function calcularMediaExame() {
     let mFinal = parseFloat(mediaFinal.value);
     let Exame = parseFloat(notaExame.value);
     let mediaExame = (mFinal + Exame) / 2;
+    let registro = {
+        media: mediaExame,
+        data: new Date().toLocaleDateString(),
+        hora: new Date().toLocaleTimeString(),
+    };
 
+    
+    
+    //adicionando o valor do imput na lista
+    historico.push(registro);
+    //mandando o valor para o localstorage
+    localStorage.setItem('mediaExameStorage', JSON.stringify(historico))
 
+    
 
     if (mediaExame >= 5.0) {
         mostrarResultado.innerHTML = "<p> Parabéns! Você foi aprovado com média: " + mediaExame.toFixed(1) + "</p>";
@@ -27,6 +42,17 @@ else if (isNaN(mediaExame) || mediaExame == null) {
     else {
         mostrarResultado.innerHTML = "<p class='mediaBaixa'> Infelizmente você foi reprovado com média: " + mediaExame.toFixed(1) + "</p>";
     }
+
+historico.forEach((registro, index) => { 
+    let resultado = registro.media >= 5.0 ? "Foi aprovado" : "Foi reprovado"
+    console.log(`Cálculo ${index + 1}: 
+        Média = ${registro.media}
+        Data = ${registro.data}
+        Hora = ${registro.hora}
+        resultado = ${resultado}`
+    ); 
+});
+
 
 }
 
